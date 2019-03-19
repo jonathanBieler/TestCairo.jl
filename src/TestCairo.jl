be386e118f2950313867f9c50161dd14a4c36400
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-03-19 16:20:52 +0100

    testing libcairo

---
+++ src/TestCairo.jl
@@ -0,0 +1,4 @@
+module TestCairo
+    using Homebrew
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using TestCairo, Homebrew, Libdl
+
+@info "installing cairo"
+Homebrew.brew(`install cairo`)
+@info "done installing cairo"
+
+@show readdir( joinpath( Homebrew.prefix(), "Cellar/cairo/1.16.0/lib/") )
+
+const _jl_libcairo = joinpath( Homebrew.prefix(), "Cellar/cairo/1.16.0/lib/","libcairo.dylib" )
+
+@show h = Libdl.dlopen_e(_jl_libcairo, Libdl.RTLD_LAZY)
+@show libcairo_version = VersionNumber(unsafe_string(ccall((:cairo_version_string,_jl_libcairo),Cstring,()) ))
+

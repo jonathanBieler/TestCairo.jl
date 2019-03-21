c66899dea414097f6934a9b22af944dfcdb8706a
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-03-21 14:43:37 +0100

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,10 +17,14 @@ const _jl_libcairo = joinpath( Homebrew.prefix(), "Cellar/cairo/1.16.0/lib/","li
     end
 end
 
-h = Libdl.dlopen_e(_jl_libcairo, Libdl.RTLD_LAZY)
-@show libcairo_version = VersionNumber(unsafe_string(ccall((:cairo_version_string,_jl_libcairo),Cstring,()) ))
+@testset "version" begin
 
-f = Libdl.dlsym_e(h, "cairo_version")
-f == C_NULL && return false
-v = ccall(f, Int32,())
-@test v > 10800
+    h = Libdl.dlopen_e(_jl_libcairo, Libdl.RTLD_LAZY)
+    @test libcairo_version = VersionNumber(unsafe_string(ccall((:cairo_version_string,_jl_libcairo),Cstring,()) )) ==  v"1.16.0"
+
+    f = Libdl.dlsym_e(h, "cairo_version")
+    f == C_NULL && return false
+    v = ccall(f, Int32,())
+    @test v > 10800
+
+end
